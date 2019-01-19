b16691dd603bf8da31b7cb470c02c0c6203f3b24
dilorom77 <dilorommulladjonova1@gmail.com>
2019-01-19 14:52:33 -0500

    fixed comfic

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,9 +1,10 @@
 public class Calculator {
 
+    //this is a very useful method
     public static double add(double num1, double num2){
         return num1 + num2;
     }
-   public static double subtract(double num1,double num2 ){
+    public static double subtract(double num1,double num2 ){
       return num1 - num2;
    }
     public static double multiply(double num1, double num2){
